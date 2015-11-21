2b8e64dfbec892ecbef7608a8b6163f67f40ad08
Sean Doyle <sean.p.doyle24@gmail.com>
2015-11-21 12:28:41 -0500

    Add Capybara Test coverage
    The original configuration had some issues with it:
    
    * `root to: "application#index"` doesn't properly hook into the EmberCLI
      ecosystem. Instead, use `mount_ember_app`:
    
    ```ruby
    mount_ember_app :exam_builder,
      to: "/",
      controller: "application",
      action: "index"
    ```
    
    * We couldn't properly reproduce the bug in this repo, since
      `bower_path: bin/bower` doesn't exist, and `jquery` and `handlebars`
      aren't included in your Sprockets configuration. We've removed them
      here for the sake of reproducing, but in your production app they can
      stick around, so long as `bower_path` points to a valid `bower`
      executable, and `jquery` and `handlebars` come from your Sprockets
      configuration
    
    * I assume your production `app/views/layouts/application.html` is more
      involved than what you've provided here, and probably includes the
      `#exam_builder` element. Unfortunately, for the sake of reproducing, I
      had to remove the `rootElement` configuration from
      `exam_builder/app/app.js`.

---
+++ config/environments/test.rb
@@ -0,0 +1,11 @@
+Rails.application.configure do
+  config.assets.raise_runtime_errors = true
+  config.cache_classes = true
+  config.eager_load = false
+  config.serve_static_files   = true
+  config.static_cache_control = 'public, max-age=3600'
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+  config.action_dispatch.show_exceptions = false
+  config.active_support.test_order = :random
+end
